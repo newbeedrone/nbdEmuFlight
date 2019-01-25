c27fa59f45aa664c1df8b5ad2067d269265e4470
czchc <chenhaicong@hotmail.com>
2019-01-25 15:58:26 +0800

    add BeeBrain BL

---
+++ src/main/target/BEEBRAIN_BL/target.mk
@@ -0,0 +1,10 @@
+F411_TARGETS += $(TARGET)
+FEATURES     = VCP ONBOARDFLASH
+
+TARGET_SRC  =                                       \
+             drivers/accgyro/accgyro_mpu.c          \
+             drivers/accgyro/accgyro_mpu6500.c      \
+             drivers/accgyro/accgyro_spi_mpu6500.c  \
+             drivers/max7456.c                      \
+             drivers/vtx_rtc6705_soft_spi.c			\
+			 drivers/flash_m25p16.c
